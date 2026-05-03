afd87224f8e71a4cb868810199e7eb3aa1624a4a
zdc <3170877204@qq.com>
2026-05-03 12:04:12 +0800

    doc: 在README中添加更新日志部分

--- GateServer/src/test.cpp
+++ GateServer/src/test.cpp
@@ -12,7 +12,6 @@ int main()
 	unsigned short gate_port = atoi(gate_port_str.c_str());
 	try
 	{
-		unsigned short port = static_cast<unsigned short>(8080);
 		net::io_context ioc{ 1 };
 		boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
 		signals.async_wait([&ioc](const boost::system::error_code& error, int signal_number) {
@@ -22,8 +21,9 @@ int main()
 			}
 			ioc.stop();
 			});
-		std::make_shared<CServer>(ioc, port)->Start();
-		std::cout << "Gate Server listen on port: " << port << std::endl;
+			
+		std::make_shared<CServer>(ioc, gate_port)->Start();
+		std::cout << "Gate Server listen on port: " << gate_port << std::endl;
 		ioc.run();
 	}
 	catch (std::exception const& e)
